37179572abdffdbecab49ab2d8c3d3fd3e025e75
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:25:57 +0530

    {Ayush Gupta} add : Q31_LeapYearIfElse.java

--- Control Flows/Q31_LeapYearIfElse.java
+++ Control Flows/Q31_LeapYearIfElse.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q31_LeapYearIfElse {
+class Q31_LeapYearIfElse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
