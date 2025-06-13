72dceb4a7d0541c945888b07065322f939e2f419
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:30:22 +0530

    Update Q8_FactorialWhile.java

--- Control Flows/Q8_FactorialWhile.java
+++ Control Flows/Q8_FactorialWhile.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q8_FactorialWhile {
+class Q8_FactorialWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
