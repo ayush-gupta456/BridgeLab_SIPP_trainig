bcfc2b9a9a7ef744ca107373d59b0ed3ead1e07f
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:25:33 +0530

    {Ayush Gupta} add : Q30_LeapYear.java

--- Control Flows/Q30_LeapYear.java
+++ Control Flows/Q30_LeapYear.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q30_LeapYear {
+class Q30_LeapYear {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
