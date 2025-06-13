86d1846a0d8c225504124f7f17cb30100287aa7a
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:23:50 +0530

    {Ayush Gupta} add : Q25_MultiplicationTable.java

--- Control Flows/Q25_MultiplicationTable.java
+++ Control Flows/Q25_MultiplicationTable.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q25_MultiplicationTable {
+class Q25_MultiplicationTable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
