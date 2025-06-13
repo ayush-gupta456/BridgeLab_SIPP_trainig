ad6ef5e1714c448c9a4cfd802f5d12f535d91e97
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:17:15 +0530

    {Ayush Gupta} Update Q13_DivisibleByFive.java

--- Control Flows/Q13_DivisibleByFive.java
+++ Control Flows/Q13_DivisibleByFive.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q13_DivisibleByFive {
+class Q13_DivisibleByFive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
