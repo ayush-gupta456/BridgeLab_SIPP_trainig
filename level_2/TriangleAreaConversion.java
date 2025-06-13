c170a4ff93ebe0d6184f88008537f10304588175
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:32:15 +0530

    {Ayush Gupta} add : TriangleAreaConversion.java

--- level_2/TriangleAreaConversion.java
+++ level_2/TriangleAreaConversion.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TriangleAreaConversion {
+class TriangleAreaConversion {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double base = input.nextDouble();
