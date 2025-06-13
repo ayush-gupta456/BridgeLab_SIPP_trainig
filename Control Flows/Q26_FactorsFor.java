a8b122af8bb09ea3be92ed2423f21d772166e652
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:24:08 +0530

    {Ayush Gupta} add : Q26_FactorsFor.java

--- Control Flows/Q26_FactorsFor.java
+++ Control Flows/Q26_FactorsFor.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q26_FactorsFor {
+class Q26_FactorsFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
