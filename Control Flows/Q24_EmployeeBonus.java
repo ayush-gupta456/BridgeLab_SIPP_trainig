0d1014d212d32ad076b7469e841e9fac620cc096
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:23:25 +0530

    {Ayush Gupta} add : Q24_EmployeeBonus.java

--- Control Flows/Q24_EmployeeBonus.java
+++ Control Flows/Q24_EmployeeBonus.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q24_EmployeeBonus {
+class Q24_EmployeeBonus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double salary = sc.nextDouble();
