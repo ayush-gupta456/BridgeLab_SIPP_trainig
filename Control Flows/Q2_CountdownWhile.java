55fb4a318f34be03ac1ddc26596f4d0b10dbc5f6
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:25:18 +0530

    {Ayush Gupta} add : Q2_CountdownWhile.java

--- Control Flows/Q2_CountdownWhile.java
+++ Control Flows/Q2_CountdownWhile.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q2_CountdownWhile {
+ class Q2_CountdownWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int counter = sc.nextInt();
