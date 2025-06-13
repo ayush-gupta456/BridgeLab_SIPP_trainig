25f436e97045ad9177715414909835f1a23038fc
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:27:45 +0530

    {Ayush Gupta} add : Q37_SwitchCalculator.java

--- Control Flows/Q37_SwitchCalculator.java
+++ Control Flows/Q37_SwitchCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q37_SwitchCalculator {
+class Q37_SwitchCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble(), b = sc.nextDouble();
