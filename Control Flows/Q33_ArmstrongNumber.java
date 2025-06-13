dc3264d616cea91f40e69adca4a249702719271a
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:26:35 +0530

    {Ayush Gupta} add : Q33_ArmstrongNumber.java

--- Control Flows/Q33_ArmstrongNumber.java
+++ Control Flows/Q33_ArmstrongNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q33_ArmstrongNumber {
+class Q33_ArmstrongNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), original = number, sum = 0;
