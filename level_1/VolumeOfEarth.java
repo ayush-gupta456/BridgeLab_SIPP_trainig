5d0e93ba3b6ef975ffa40c4411c4eee853d48a99
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:31:52 +0530

    {Ayush Gupta} add : VolumeOfEarth.java

--- level_1/VolumeOfEarth.java
+++ level_1/VolumeOfEarth.java
@@ -1,5 +1,5 @@
 class VolumeOfEarth {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         double radiusKm = 6378;
         double volumeKm = (4.0/3) * Math.PI * Math.pow(radiusKm, 3);
         double volumeMiles = volumeKm / Math.pow(1.6, 3);
