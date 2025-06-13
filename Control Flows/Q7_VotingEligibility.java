43fa6f12aba63ed662b0693d7283101d23ff6690
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:30:07 +0530

    {Ayush Gupta} add : Q7_VotingEligibility.java

--- Control Flows/Q7_VotingEligibility.java
+++ Control Flows/Q7_VotingEligibility.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q7_VotingEligibility {
+class Q7_VotingEligibility {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
