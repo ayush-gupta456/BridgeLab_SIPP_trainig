b1caa9b24656f4d0902ae16b4350898c5a4d9017
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:20:59 +0530

    {Ayush Gupta} add : Q20_FactorsWhile.java

--- Control Flows/Q20_FactorsWhile.java
+++ Control Flows/Q20_FactorsWhile.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q20_FactorsWhile {
+ class Q20_FactorsWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), i = 1;
