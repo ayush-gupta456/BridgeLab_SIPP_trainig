28a7846fcfe8ef20ca57a5b6cdf664efe4bd8fd4
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:15:58 +0530

    {Ayush Gupta} Update Q1_SumNaturalNumbers.java

--- Control Flows/Q1_SumNaturalNumbers.java
+++ Control Flows/Q1_SumNaturalNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-public class Q1_SumNaturalNumbers {
-    public static void main(String[] args) {
+class Q1_SumNaturalNumbers {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         if (n > 0) {
