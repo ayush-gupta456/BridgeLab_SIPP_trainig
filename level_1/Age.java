3a192b799f69bfc141feb7c3205805bd29f516f3
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-13 10:37:24 +0530

    {Ayush Gupta} add : Age.java

--- level_1/Age.java
+++ level_1/Age.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.time.*;
 class Age{
-public static void main(String... args){
+public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter your birth year :");
 int byear=sc.nextInt();
